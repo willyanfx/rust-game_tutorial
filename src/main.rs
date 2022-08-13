c21df20edf61d57e5ae685e4cf58c056fc591a1e
willyanfx <willyan.fx@gmail.com>
2022-08-13 11:11:45 -0700

    added state

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use rusty_engine::prelude::*;
 
+struct GameState {
+    high_score: u32,
+    current_score: u32,
+    enemy_label: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            high_score: 0,
+            current_score: 0,
+            enemy_label: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
 fn main() {
     let mut game = Game::new();
-    println!("Hello, world!");
 
-    game.run(());
+    //  setup game here
+
+    game.run(GameState::default());
 }
