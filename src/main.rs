43a9148a325f2f4be14a1bf451c31ed964ba5dea
willyanfx <willyan.fx@gmail.com>
2022-08-16 19:42:25 -0700

    colliders

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rusty_engine::prelude::*;
+use rusty_engine::prelude::{bevy::sprite::collide_aabb::Collision, *};
 
 struct GameState {
     high_score: u32,
@@ -25,12 +25,29 @@ fn main() {
     player.translation = Vec2::new(0.0, 0.0);
     player.rotation = std::f32::consts::FRAC_PI_2;
     player.scale = 1.0;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    game_state.current_score += 1;
-    println!("Current score {}", game_state.current_score);
+    for event in engine.collision_events.drain(..) {
+        if event.state == Collision::Right && event.pair.one_starts_with("player") {
+            for label in [event.pair.0, event.pair.1] {
+                if label != "player" {
+                    engine.sprites.remove(&label);
+                }
+            }
+            game_state.current_score += 1;
+            println!("Current score {}", game_state.current_score);
+        }
+    }
+
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
 }
